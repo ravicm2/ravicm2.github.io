ad668fbdfdf4a58349efddb3f21893705a05f4e6
Ravichandran M <ravichandran.m@capgemini.com>
2020-08-19 12:30:02 +0530

    disabled right click

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
